86195c80a13f21dea23c54f2010e001cf045e1cd
Jeneth Diesta <jen6one9@gmail.com>
2020-11-11 20:38:39 -0800

    updated index.js

--- employee/src/components/DataArea.js
+++ employee/src/components/DataArea.js
@@ -1,20 +1,67 @@
 import React from 'react';
 import axios from 'axios';
-
-class DataArea extends React.Component{
-    state= {
+import { MDBDataTable } from 'mdbreact'
+class DataArea extends React.Component {
+    state = {
         employees: []
     }
     componentDidMount = () => {
         axios.get("https://randomuser.me/api/?results=200&nat=us")
-        .then(records =>{
-            console.log(records)
-        })
+            .then(records => {
+                console.log(records)
+                let APIdata = records.data.results
+                let employees = []
+                for (let i = 0; i < APIdata.length; i++) {
+                    var emp = {
+
+                        name: APIdata[i].name.title + APIdata[i].name.first + " " + APIdata[i].name.last,
+                        email: APIdata[i].email,
+                        phone: APIdata[i].phone,
+                        DOB: APIdata[i].dob.date,
+                        location: APIdata[i].location.city + ", " + APIdata[i].location.street,
+                    }
+                    employees.push(emp)
+
+                }
+                this.setState({ employees: employees })
+                console.log(this.state.employees)
+            })
     }
-    render(){
-        return(<div>
-            <h1>Employee Data</h1>
-        </div>)
+    render() {
+        let dataArea = {
+            columns: [
+                {
+                    label: "name",
+                    field: "name",
+                    width: 200,
+                },
+                {
+                    label: "email",
+                    field: "email",
+                    width: 200,
+                },  
+                {
+                    label: "phone",
+                    field: "phone",
+                    width: 200,
+                },
+                {
+                    label: "DOB",
+                    field: "DOB",
+                    width: 200,
+                },  
+                {
+                    label: "location",
+                    field: "location",
+                    width: 200,
+                }         
+            ],
+            rows: this.state.employees
+    }
+    return(<div>
+        <h1>Employee Data</h1>
+        <MDBDataTable data ={dataArea}> </MDBDataTable>
+        </div >)
     }
 }
 
